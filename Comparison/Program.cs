562258578d6a109663b2b619b52209e09bbad8d0
oscar <oscar@o2webdev.com>
2019-07-18 19:30:59 -0400

    Check given strings are equal or not.

---
+++ Comparison/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IncludeHelp
+{
+    class Test
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+            string str1;
+            string str2;
+
+            //input strings
+            Console.Write("Enter a string: ");
+            str1 = Console.ReadLine();
+            Console.Write("Enter another string: ");
+            str2 = Console.ReadLine();
+
+            //comparing strings 
+            if (str1 == str2)
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            //another way 
+            if ((str1 == str2) == true)
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            //comparing by ignoring the case 
+            //convert both strings in the same case 
+            //either in uppercase or lowercase
+            Console.WriteLine("By ignoring case...");
+            if (str1.ToUpper() == str2.ToUpper())
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            //Hit ENTER to exit the program
+            Console.ReadLine();
+        }
+    }
+}
